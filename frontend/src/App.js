6217c9752fb4c21fc342aab92fc578a8a2b6775f
yuvraj-kalsi0001 <97918056+yuvraj-kalsi0001@users.noreply.github.com>
2023-09-23 15:15:40 -0230

    update
    working on local storage, but not workinng on php

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,58 @@
-import "./App.css";
 import Header from "./myComponents/Header.js";
 import { About } from "./myComponents/About.js";
-import React, { useState } from "react";
 import Login from "./Login.js";
 import Signup from "./Signup.js";
 import Home from "./Home";
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import CreateTodo from "./CreateTodo";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { AddTodo } from "./myComponents/AddTodo.js";
+import Todos from "./myComponents/Todos.js";
+
 function App() {
+  let initTodo;
 
+  if (window.localStorage.getItem("todos") === null) {
+    initTodo = [];
+  } else {
+    initTodo = JSON.parse(window.localStorage.getItem("todos"));
+  }
 
+  const onDelete = (todo) => {
+    console.log("onDelete is working of todo:", todo);
+    setTodos(
+      todos.filter((e) => {
+        return e !== todo;
+      })
+    );
 
+    window.localStorage.setItem("todos", JSON.stringify(todos));
+  };
 
-  // if (window.localStorage.getItem("todos") === null) {
-  //   initTodo = [];
-  // } else {
-  //   initTodo = JSON.parse(window.localStorage.getItem("todos"));
-  // }
+  const addTodo = (title, desc) => {
+    let Sno;
+    console.log("I am adding this todo");
+    if (todos.length === 0) {
+      Sno = 1;
+    } else {
+      Sno = todos[todos.length - 1].Sno + 1;
+    }
 
+    const myTodo = {
+      Sno: Sno,
+      title: title,
+      description: desc,
+    };
+    console.log(myTodo);
+    setTodos([...todos, myTodo]);
+  };
+
+  const [todos, setTodos] = useState(initTodo);
+
+  useEffect(() => {
+    window.localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
 
   return (
@@ -25,9 +60,11 @@ function App() {
 
       <Routes>
         <Route exact path="/" element={<Login />}></Route>
-        <Route exact path="/home" element={
-          <><Header titleName="My Todos List" searchBar={true} />
-            <Home /></>}
+        <Route exact path="/home" element={<>
+                <AddTodo addTodo={addTodo} />
+                <Todos todos={todos} onDelete={onDelete} />
+                {/* <About /> */}
+              </>}
 
         ></Route>
         <Route exact path="/signup" element={<Signup />}></Route>
